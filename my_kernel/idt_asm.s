731f60698e32b7a396c9ce808a90afd06680632a
Alan <147257618+alanysfgl@users.noreply.github.com>
2026-02-14 15:10:11 +0300

    Expand interrupt handling with full ISRs and timer IRQ

--- my_kernel/idt_asm.s
+++ my_kernel/idt_asm.s
@@ -4,3 +4,4 @@ idt_flush:
     mov eax, [esp+4]  ; IDT adresi
     lidt [eax]        ; IDT'yi yükle
     ret
+section .note.GNU-stack noalloc noexec nowrite progbits
--- my_kernel/kernel.c
+++ my_kernel/kernel.c
@@ -1,11 +1,16 @@
 #include "idt.h"
 #include "gdt.h"
 
+extern void monitor_write(char *text);
+
 void kernel_main() {
     init_gdt();
-    init_idt(); 
-    
-    while(1) {
+    init_idt();
+
+    monitor_write("Learning-OS kernel baslatildi.\n");
+    monitor_write("IRQ0 (timer) ve IRQ1 (keyboard) aktif.\n");
+
+    while (1) {
         asm volatile("hlt");
     }
 }
